d0fa480997e3fcd3b1e4f447f9a3858f86b98c0e
Jeff Greer <geergan@gmail.com>
2026-07-18 20:03:59 -0700

    refactor

--- src/slim.cpp
+++ src/slim.cpp
@@ -1,4 +1,4 @@
-#include <filesystem>
+#include <print>
 #include <future>
 #include <string>
 #include "config.h"
@@ -10,20 +10,22 @@
 #include <slim/service/launcher.h>
 #include <slim/slim.h>
 
+namespace slim {
 namespace {
-static void slim::initialize_ssl() {
+static void initialize_ssl() {
     SSL_library_init();
     SSL_load_error_strings();
     OpenSSL_add_all_algorithms();
 }
 } // namespace
 
-void slim::start() {
+void start() {
     initialize_ssl();
 	auto launch_script_future = std::async(std::launch::async, slim::service::launcher::launch, slim::command_line::get_script_name());
 	launch_script_future.get();
 }
-void slim::version() {
-	log::info("slim:  " VERSION);
-	log::info("libv8:  " + std::string(v8::V8::GetVersion()));
+void version() {
+    std::println("slim:  {}", VERSION);
+	std::println("libv8: {} ", v8::V8::GetVersion());
 }
+} // namespace slim
