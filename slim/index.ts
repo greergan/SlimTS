64c30176380d86dca3c9d0cd100376a11791c761
Jeff Greer <geergan@gmail.com>
2026-07-22 21:14:14 -0700

    temp area to create webserver app

---
+++ slim/index.ts
@@ -0,0 +1,16 @@
+import console from 'console';
+import server from 'http';
+console.trace("begin");
+const config: SlimHttpServerConfig = { host: '0.0.0.0', port: 9000 };
+async function serve(config: SlimHttpServerConfig) {
+    console.debug(config);
+    const listener = server.serve(config);
+    console.debug("listener created");
+    for await (const event of listener) {
+        console.debug("event found");
+         //const url = new URL(event.request.url);
+         //console.debug(event.request.url);
+     }
+}
+serve(config);
+console.trace("end");
