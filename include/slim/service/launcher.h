cf5a2a297c20fd45eb9f6f0cfcc45b94e2ce91af
Jeff Greer <geergan@gmail.com>
2026-07-22 20:53:58 -0700

    refactor: remove marshal_resources

--- include/slim/service/launcher.h
+++ include/slim/service/launcher.h
@@ -1,11 +1,6 @@
-#ifndef __SLIM__SERVICE__LAUNCHER__H
-#define __SLIM__SERVICE__LAUNCHER__H
-#include <v8.h>
-#include <string>
-#include <vector>
-#include <slim/module/import_specifier.h>
+#pragma once
+#include <string_view>
 namespace slim::service::launcher {
-	void marshal_resources();
-	void launch(slim::module::variant_specifier _script_name_string_or_specifier_stub);
+	void launch(std::string_view specifier_uri);
 }
-#endif
+
