90b4ceb1aa225a15e8f2b3f487e1b8fe62af950f
Jeff Greer <geergan@gmail.com>
2026-07-22 20:39:23 -0700

    add debug, trace

--- types/console.d.ts
+++ types/console.d.ts
@@ -1,7 +1,9 @@
 declare module 'console' {
     const console: {
         log(...args: any[]): void;
+        debug(...args: any[]): void;
         error(...args: any[]): void;
+        trace(...args: any[]): void;
     };
     export default console;
 }
