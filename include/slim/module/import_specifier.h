db4f1d9504ff68080d71ebbc257377f61035dff0
Jeff Greer <geergan@gmail.com>
2026-08-03 16:17:05 -0700

    feature: add slim::fetch for .mjs files fix: various cleanup

--- include/slim/module/import_specifier.h
+++ include/slim/module/import_specifier.h
@@ -5,6 +5,7 @@
 #include <v8.h>
 #include <libtsgo.h>
 #include <tsgo.h>
+#include <slim/common/http/response.h>
 namespace slim::module {
 	class import_specifier {
 	    public:
@@ -18,16 +19,17 @@ namespace slim::module {
 		private:
 			bool is_entry_point_ = false;
 			bool is_synthetic_module_ = false;
+			bool is_src_transpiled = false;
 			v8::Isolate* isolate_;
 			v8::Local<v8::Context> context_;
 			v8::Local<v8::Module> v8_module_;
 			v8::Local<v8::Module> referrer_;
 			std::string specifier_uri_;
-			std::string compiled_specifier_string_url_;
 			std::string specifier_protocol_;
 			std::filesystem::path cache_directory_path_;
 			std::filesystem::path specifier_path_;
-			GoStr transpiled_source_code_;
+			GoStr transpiled_source_;
+			slim::common::http::Response fetched_mjs_source_;
 			void resolve_module_path(std::string_view specifier);
 			void specifier_uri(std::string_view s);
 	};
